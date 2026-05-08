b73a9fd70e281a0487a4a1a00e70e40d68d12658
ypires <yessicapire02@gmail.com>
2026-05-08 17:18:42 -0400

    proyecto final python

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.db import IntegrityError
 from .forms import SignupForm, EditarUsuarioForm, EditarPerfilForm, CambiarPasswordForm
 from .models import Perfil
 
@@ -10,11 +11,14 @@ def signup_view(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            Perfil.objects.create(usuario=user)
-            login(request, user)
-            messages.success(request, f'¡Bienvenida, {user.username}! Tu cuenta fue creada exitosamente.')
-            return redirect('index')
+            try:
+                user = form.save()
+                Perfil.objects.create(usuario=user)
+                login(request, user)
+                messages.success(request, f'¡Bienvenida, {user.username}! Tu cuenta fue creada exitosamente.')
+                return redirect('index')
+            except IntegrityError:
+                form.add_error('username', 'Ya existe un usuario con ese nombre. Elegí otro.')
     else:
         form = SignupForm()
     return render(request, 'accounts/signup.html', {'form': form})
